e1e358014eaa1e470e097d15f2df706e4fda87fd
Lucas <lucas_gomesdesouza@outlook.com>
2025-04-17 11:42:58 -0300

    Organizing folders

---
+++ MicroservicesDotNet/MicroservicesDotNet/Program.cs
@@ -0,0 +1,24 @@
+using MicroservicesDotNet.Services;
+using MicroservicesDotNet.Services.Implementations;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Dependency Injection
+
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ MicroservicesDotNet/MicroservicesDotNet/Services/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using MicroservicesDotNet.Model;
+
+namespace MicroservicesDotNet.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
